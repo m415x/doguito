eabfe71bcd6106747f36ab250b3ffdc234ba697c
Cristian Lahoz <lahozcristian@gmail.com>
2023-08-28 20:58:47 -0300

    clase 02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Header from './components/Header';
 import Home from "./pages/Home"
+import Page404 from './pages/Page404';
 import Sobre from "./pages/Sobre"
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
@@ -8,9 +10,11 @@ function App() {
 
   return (
     <Router>
+      <Header/>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/sobre" element={<Sobre />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
   );
---
+++ src/components/ListPosts.jsx
@@ -0,0 +1,7 @@
+import "../assets/css/componentes/card.css"
+
+const ListPosts = ({ url }) => {
+  return <section className="posts container"></section>
+}
+
+export default ListPosts
