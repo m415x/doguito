7e1b78ad159c891c59b6f2248d8d7910f8913e66
Cristian Lahoz <lahozcristian@gmail.com>
2023-08-30 19:56:36 -0300

    clase 05

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Home from "./pages/Home"
 import Page404 from './pages/Page404';
 import Sobre from "./pages/Sobre"
 import Post from "./pages/Post"
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Categoria from './pages/Categoria';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/posts/:id" element={<Post />} />
+        <Route path='/categoria/:id/*' element={<Categoria />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
